a15aa8b0e84e90645794c91fb7f135845f9bd5bd
Javier Roncallo <jroncallo96@gmail.com>
2018-09-13 01:31:27 -0500

    Se elimina el metodo sendFile y se crea el metodo createFile en el web service

--- MyWebService/src/java/org/me/webservice1/WebService1.java
+++ MyWebService/src/java/org/me/webservice1/WebService1.java
@@ -5,6 +5,7 @@
  */
 package org.me.webservice1;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import javax.jws.WebService;
@@ -27,18 +28,16 @@ public class WebService1 {
     public String hello(@WebParam(name = "name") String txt) {
         return "Hello " + txt + " !";
     }
-    
-    @WebMethod(operationName = "sendFile")
-    public String sendFile(
-            @WebParam(name = "fileName") String fileName,
-            @WebParam(name = "bytes") byte[] bytes){
+
+    @WebMethod(operationName = "createFile")
+    public boolean createFile(@WebParam(name = "fileName") String fileName){
         try {
             String path = "/home/javier/Desktop/test/server/"+fileName;
-            FileOutputStream fos = new FileOutputStream(path);
-            fos.write(bytes);
-            return path;
+            File file = new File(path);
+            file.createNewFile();
+            return true;
         } catch (IOException ex) {
-            return "ERROR";
+            return false;
         }
     }
 }
